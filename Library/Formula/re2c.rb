d67a1b88775ad18ca3ef9dc435299b50d9da4ee2
Andrey Nikiforov <a.d.nikiforov@gmail.com>
2014-12-16 00:11:55 +0300

    Add version, add worked zip url

--- Library/Formula/re2c.rb
+++ Library/Formula/re2c.rb
@@ -1,15 +1,16 @@
 require "formula"
 
 class Re2c < Formula
-  homepage "http://re2c.org"
-  url "https://downloads.sourceforge.net/project/re2c/re2c/0.13.7.5/re2c-0.13.7.5.tar.gz"
-  sha1 "4786a13be61f8249f4f388e60d94bb81db340d5c"
+  homepage "https://github.com/bproctor/re2c"
+  url "https://github.com/bproctor/re2c/archive/master.zip"
+  sha1 "aa297929b1ee7d11b04b60f21661d910a16bdd66"
+  version "1.3.5"
 
   bottle do
     cellar :any
-    sha1 "a3246b77461757a1d03fce798dc0e96946dd8d4b" => :yosemite
-    sha1 "295edf3bf8132c990a2d524132ce1cf0f4e22c38" => :mavericks
-    sha1 "b289675f0bc4e76b1f131e82a7fe41f4d11804ed" => :mountain_lion
+    sha1 "aa297929b1ee7d11b04b60f21661d910a16bdd66" => :yosemite
+    sha1 "aa297929b1ee7d11b04b60f21661d910a16bdd66" => :mavericks
+    sha1 "aa297929b1ee7d11b04b60f21661d910a16bdd66" => :mountain_lion
   end
 
   def install
